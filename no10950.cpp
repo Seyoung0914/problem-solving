50b30216f362f2f8caf91c37033cd796a0a7bad7
seyoung <seyoung8529@gmail.com>
2025-03-01 08:26:18 +0900

    +no10950.cpp

--- no10950.cpp
+++ no10950.cpp
@@ -1,6 +1,6 @@
 // A+B - 3 성공
 // 시간 제한	메모리 제한	제출	정답	맞힌 사람	정답 비율
-// 1 초	256 MB	350251	204936	170595	58.536%
+// 1 초	256 MB	351086	205466	171022	58.540%
 // 문제
 // 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
 
@@ -11,21 +11,19 @@
 
 // 출력
 // 각 테스트 케이스마다 A+B를 출력한다.
-
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
-    int a, b, num;
+    int a, b, n;
 
-    cin >> num;
+    cin >> n;
 
-    for (int i = 0; i < num; i++)
+    for (int i = 0; i < n; i++)
     {
         cin >> a >> b;
         cout << a + b << endl;
     }
-    return 0;
 }
--- no10992.cpp
+++ no10992.cpp
@@ -1,6 +1,6 @@
-// 별 찍기 - 16
+// 별 찍기 - 17
 // 시간 제한	메모리 제한	제출	정답	맞힌 사람	정답 비율
-// 1 초	256 MB	21289	16645	15032	79.033%
+// 1 초	256 MB	22825	15139	13558	67.185%
 // 문제
 // 예제를 보고 규칙을 유추한 뒤에 별을 찍어 보세요.
 
@@ -9,7 +9,6 @@
 
 // 출력
 // 첫째 줄부터 N번째 줄까지 차례대로 별을 출력한다.
-
 #include <iostream>
 
 using namespace std;
@@ -17,16 +16,24 @@ using namespace std;
 int main()
 {
     int n;
-
     cin >> n;
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n - 1; i++)
+        cout << " ";
+    cout << "*";
+    cout << endl;
+    for (int i = 0; i < n - 2; i++)
     {
-        // 공백
-        for (int k = 0; k < n - i - 1; k++)
+        for (int j = n; j > i + 2; j--)
             cout << " ";
-        // 별
-        for (int j = 0; j < i + 1; j++)
-            cout << "* ";
+        cout << "*";
+        for (int k = 0; k < 2 * (i + 1) - 1; k++)
+            cout << " ";
+        cout << "*";
         cout << endl;
+    }
+    if (n != 1)
+    {
+        for (int i = 0; i < 2 * n - 1; i++)
+            cout << "*";
     }
 }
