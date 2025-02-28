197069edb4dd1c35a12ce95beba582c4a43ba893
seyoung <seyoung8529@gmail.com>
2025-03-01 08:37:21 +0900

    **no10953

--- no10953.cpp
+++ no10953.cpp
@@ -1,6 +1,3 @@
-// A+B - 6
-// 시간 제한	메모리 제한	제출	정답	맞힌 사람	정답 비율
-// 1 초	256 MB	47314	32020	28297	68.859%
 // 문제
 // 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
 
@@ -11,12 +8,23 @@
 
 // 출력
 // 각 테스트 케이스마다 A+B를 출력한다.
-
-// #1 comma를 따로 처리해버리기!
 #include <iostream>
 
 using namespace std;
 
+int main()
+{
+    int n, a, b;
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d, %d", &a, &b);
+        printf("%d\n", a + b);
+    }
+}
+
+// 신기한 풀이
 int main()
 {
     int a, b, num;
@@ -27,27 +35,6 @@ int main()
     for (int i = 0; i < num; i++)
     {
         cin >> a >> comma >> b;
-        cout << a + b << endl;
-    }
-    return 0;
-}
-
-// #2 scanf이용하기
-
-#include <iostream>
-
-using namespace std;
-
-int main()
-{
-    int a, b, num;
-    char comma;
-
-    cin >> num;
-
-    for (int i = 0; i < num; i++)
-    {
-        scanf("%d, %d", &a, &b);
         cout << a + b << endl;
     }
     return 0;
