7bc1d5551847115538e8fb3fbc6fdccefb43c4f7
seyoung <seyoung8529@gmail.com>
2025-03-01 08:32:13 +0900

    +no10952

--- no10952.cpp
+++ no10952.cpp
@@ -21,9 +21,9 @@ using namespace std;
 int main()
 {
     int a, b;
-
-    while (cin >> a >> b)
+    while (1)
     {
+        cin >> a >> b;
         if (a == 0 && b == 0)
             break;
         cout << a + b << endl;
