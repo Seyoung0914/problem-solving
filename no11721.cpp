cb75d8eb4c279061355ac0e00124a6cefe2490b8
seyoung <seyoung8529@gmail.com>
2025-03-03 08:42:37 +0900

    **no.11721

--- no11721.cpp
+++ no11721.cpp
@@ -1,6 +1,6 @@
-// 열 개씩 끊어 출력하기
+// 열 개씩 끊어 출력하기 성공
 // 시간 제한	메모리 제한	제출	정답	맞힌 사람	정답 비율
-// 1 초	256 MB	79285	43812	37920	55.850%
+// 1 초	256 MB	79364	43865	37961	55.860%
 // 문제
 // 알파벳 소문자와 대문자로만 이루어진 길이가 N인 단어가 주어진다.
 
@@ -9,6 +9,8 @@
 // 입력
 // 첫째 줄에 단어가 주어진다. 단어는 알파벳 소문자와 대문자로만 이루어져 있으며, 길이는 100을 넘지 않는다. 길이가 0인 단어는 주어지지 않는다.
 
+// 출력
+// 입력으로 주어진 단어를 열 개씩 끊어서 한 줄에 하나씩 출력한다. 단어의 길이가 10의 배수가 아닌 경우에는 마지막 줄에는 10개 미만의 글자만 출력할 수도 있다.
 #include <iostream>
 #include <string>
 
@@ -16,46 +18,17 @@ using namespace std;
 
 int main()
 {
-    char input[101];
-
-    cin >> input;
-
-    int i = 0;
-
-    while (input[i] != '\0')
-    {
-        cout << input[i];
-        if (((i + 1) % 10) == 0)
-            cout << endl;
-        i++;
-    }
-
-    return 0;
-}
-
-int main()
-{
     string input;
+
     cin >> input;
+
+    int n = 1;
 
     for (int i = 0; i < input.length(); i++)
     {
         cout << input[i];
-
-        if ((i + 1) % 10 == 0)
+        if (n % 10 == 0)
             cout << endl;
+        n++;
     }
-    return 0;
-}
-
-// 방법 3 substring
-
-int main()
-{
-    string input;
-    cin >> input;
-
-    for (int i = 0; i < input.length(); i += 10)
-        cout << input.substr(i, 10) << endl;
-    return 0;
 }
