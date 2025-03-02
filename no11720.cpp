04a2e4ab242eb3e639a1e5e59bb6007c90b4f83c
seyoung <seyoung8529@gmail.com>
2025-03-03 08:33:30 +0900

    -no.11720

--- no11720.cpp
+++ no11720.cpp
@@ -1,6 +1,6 @@
-// 숫자의 합
+// 숫자의 합 성공
 // 시간 제한	메모리 제한	제출	정답	맞힌 사람	정답 비율
-// 1 초	256 MB	293202	163068	133406	56.041%
+// 1 초	256 MB	294154	163623	133830	56.053%
 // 문제
 // N개의 숫자가 공백 없이 쓰여있다. 이 숫자를 모두 합해서 출력하는 프로그램을 작성하시오.
 
@@ -9,42 +9,19 @@
 
 // 출력
 // 입력으로 주어진 숫자 N개의 합을 출력한다.
-
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
-    int number, sum = 0;
+    int n, sum = 0;
     char input[101];
 
-    cin >> number;
+    cin >> n;
     cin >> input;
 
-    for (int i = 0; i < number; i++)
-    {
-        sum += input[i] - '0';
-    }
-
-    cout << sum << endl;
-    return 0;
-}
-
-#include <iostream>
-#include <string>
-
-using namespace std;
-
-int main()
-{
-    string input;
-    int number, sum = 0;
-
-    cin >> number;
-    cin >> input;
-
-    for (int i = 0; i < number; i++)
+    for (int i = 0; i < n; i++)
     {
         sum += input[i] - '0';
     }
