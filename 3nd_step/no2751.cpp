46fbfddbf86c9220260123e33ff2cb24ad40113f
seyoung0914 <seyoung8529@gmail.com>
2026-02-03 13:27:25 +0900

    *no.2751-mergesort*

--- 3nd_step/no2751.cpp
+++ 3nd_step/no2751.cpp
@@ -2,34 +2,57 @@
 
 using namespace std;
 
+void mergeee(int arr[], int tmp[], int start, int mid, int end);
+void mergesort(int arr[], int tmp[], int start, int end){
+    if(start < end){
+        int mid = (start+end)/2;
+        mergesort(arr, tmp, start, mid);
+        mergesort(arr, tmp, mid+1, end);
+        mergeee(arr, tmp, start, mid, end);
+    }
+    
+}
+void mergeee(int arr[], int tmp[], int start, int mid, int end){
+    // tmp에 복사해줌
+    for(int i=start; i<=end; i++){
+        tmp[i] = arr[i];
+    }
+
+    int start1 = start;
+    int start2 = mid+1;
+    int index = start;
+
+    while(start1<=mid && start2<=end){
+        if(tmp[start1]<tmp[start2]){
+            arr[index] = tmp[start1];
+            start1++;
+        }
+        else{
+            arr[index] = tmp[start2];
+            start2++;
+        }
+        index++;
+    }
+    for(int i=0; i<= mid-start1; i++){
+        arr[index+i] = tmp[start1+i];
+    }
+}
+
 int main(){
 
     int n, temp;
 
     cin >> n;
 
-    int list[n];
+    int arr[n], tmp[n];
+
+    for(int i=0; i<n; i++) cin >> arr[i];
+
+    mergesort(arr, tmp, 0, n-1);
 
     for(int i=0; i<n; i++){
-        cin >> list[i];
-    }
-
-    // 정렬 시작
-    for(int i=0; i<n-1; i++){
-        for(int j=0; j<n-1-i; j++){
-            // swap
-            if(list[j+1]<list[j]){
-                temp = list[j];
-                list[j] = list[j+1];
-                list[j+1] = temp;
-            }
-        }
-    }
-
-    for(int i=0; i<n; i++){
-        cout << list[i] << " ";
+        cout << arr[i] << " ";
     }
     cout << endl;
-    
-    return 0;
+
 }
