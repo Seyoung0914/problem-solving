51debfd35e00761695f3df2d221b7b1794e72e3c
seyoung <seyoung8529@gmail.com>
2026-02-13 19:54:31 +0900

    *10989*

--- 3nd_step/no10989.cpp
+++ 3nd_step/no10989.cpp
@@ -8,24 +8,22 @@ int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
+    cout.tie(NULL);
 
     int n, num;
-
+    int number[10001] = {0};
     cin >> n;
 
-    vector<int> number;
-
-    for (int i = 0; i < n; i++)
+    for (int i = 1; i <= n; i++)
     {
         cin >> num;
-        number.push_back(num);
+        number[num]++;
     }
 
-    sort(number.begin(), number.end());
-
-    for (int i = 0; i < n; i++)
+    for (int i = 1; i <= 10000; i++)
     {
-        cout << number[i] << '\n';
+        for (int j = 1; j <= number[i]; j++)
+            cout << i << '\n';
     }
 
     return 0;
